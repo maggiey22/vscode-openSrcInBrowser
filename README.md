92da9254be09c03f07b193b3ea967ed2e5fe92dd
Maggie <43491401+maggiey22@users.noreply.github.com>
2020-12-30 01:21:52 -0800

    add main features

---
+++ README.md
@@ -0,0 +1,36 @@
+# Open Source Code in Browser - VSCode Extension
+
+Quickly open source code for the current file or folder in the browser.
+
+<img src="https://github.com/maggiey22/vscode-openSrcInBrowser/blob/main/pics/1.gif" height=210
+  title="initial setup + opening file in browser">
+<img src="https://github.com/maggiey22/vscode-openSrcInBrowser/blob/main/pics/2.gif" height=210
+  title="opening folder in browser">
+
+## Commands
+
+* Open source code in browser
+* Edit repo root URL
+
+## Run it from
+
+* command palette (`ctrl`/`cmd` + `shift` + `p` or `View` > `Command Palette...`)
+* context menu (right click in Explorer sidebar or on a file)
+* `ctrl` + `alt` + `g`
+
+## Extension settings
+
+`openSrcInBrowser.rootURL`: URL where the repository root is found (ex. https://github.com/maggiey22/discord-dad-bot).
+
+-----------------------------------------------------------------------------------------------------------
+
+## Made with
+
+* [Your First Extension - VSCode Tutorial](https://code.visualstudio.com/api/get-started/your-first-extension)
+* [VS Code Extension Generator](https://www.npmjs.com/package/generator-code)
+
+## Future plans
+
+* Open on specific line number
+* Open multiple at once
+* Publish to VSCode Marketplace!
---
+++ src/extension.ts
@@ -0,0 +1,73 @@
+import * as fs from 'fs';
+import { URL } from 'url';
+import { commands, env, window, workspace, ExtensionContext, Uri } from 'vscode';
+
+const extName = 'openSrcInBrowser';
+
+// Returns the configured root URL of the remote repo; prompts user to set it if none exists or if edit=True.
+async function getRootURL(edit = false) {
+	const config = workspace.getConfiguration(extName);
+	const rootURLconfig : string = config.get('rootURL');
+
+	if (rootURLconfig === '' || edit) {
+		const newURL = await window.showInputBox({ placeHolder: 'Set the root URL where your code is hosted' });
+		try {
+			const url = new URL(newURL);
+			config.update('rootURL', newURL);
+			return url;
+		} catch (err) {
+			window.showErrorMessage(`${newURL} is not a valid URL.`);
+			return undefined;
+		}
+	} else {
+		return new URL(rootURLconfig);
+	}
+}
+
+// Called when the extension is activated
+export function activate(context: ExtensionContext) {
+
+	console.log('🎉 Extension "Open Source Code in Browser" is now active!');
+
+	// Command to open the current file in the remote.
+	let openCmd = commands.registerCommand(`${extName}.open`, async (uri : Uri) => {
+		// The code you place here will be executed every time your command is executed
+
+		const rootURL = await getRootURL();
+		const currDocument = window.activeTextEditor.document;
+
+		if (!currDocument.isUntitled && workspace.workspaceFolders && rootURL) {
+			const currURI = (uri) ? uri.fsPath : currDocument.uri.fsPath;
+			const currRelativePath = currURI.replace(workspace.workspaceFolders[0].uri.fsPath, '');
+
+			let url = `${rootURL.toString()}`;
+			if (rootURL.hostname === 'github.com') {
+				// this is actually not necessary since blob redirects to tree when needed and vice versa
+				const isFile = (await fs.promises.lstat(currURI)).isFile();
+				url += (isFile) ? '/blob/master' : '/tree/master';
+			}
+
+			window.showInformationMessage(`Opening in browser: ${Uri.parse(`${url}${currRelativePath}`)}`);
+			env.openExternal(Uri.parse(`${url}${currRelativePath}`));
+		} else {
+			window.showInformationMessage('Nothing to open.');
+		}
+	});
+
+	// Command to edit the root URL of the remote repo
+	let editRootURLCmd = commands.registerCommand(`${extName}.editRootURL`, async () => {
+		const newURL = await getRootURL(true);
+		if (newURL) {
+			window.showInformationMessage(`Root URL was changed to ${newURL.toString()}.`);
+		} else {
+			window.showErrorMessage('Root URL was not changed.');
+		}
+	});
+
+	// Ensure commands are de-registered when extension is deactivated
+	context.subscriptions.push(openCmd);
+	context.subscriptions.push(editRootURLCmd);
+}
+
+// Called when extension is deactivated
+export function deactivate() {}
